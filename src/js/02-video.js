45aa58ac56d9227ed2f798905dd3a266ebbbd733
Viacheslav Vinichenko <fortunebetting@gmail.com>
2023-02-03 23:23:38 +0200

    UPDATE 02 VIDEO

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,11 +3,15 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const player = new Player(document.querySelector('iframe'));
+
 setPlayerTime(player, localStorage.getItem('videoplayer-current-time'));
+
 player.on('timeupdate', throttle(savePlayerTime, 1000));
 function savePlayerTime({ seconds }) {
   localStorage.setItem('videoplayer-current-time', seconds);
 }
 function setPlayerTime(currentPlayer, currentTime) {
-  currentPlayer.setCurrentTime(currentTime);
+  if (localStorage.getItem('videoplayer-current-time')) {
+    currentPlayer.setCurrentTime(currentTime);
+  }
 }
